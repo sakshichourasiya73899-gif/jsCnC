f9dc26635ecdb484ddd3f6a58a95907d2829faec
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-05-27 02:26:10 +0530

    Memory

---
+++ stackAndheap.js
@@ -0,0 +1,23 @@
+//Stack and Heap memory
+//Stack (Primitive)
+//Heap (Non-Primitive)
+
+;
+
+
+
+let name = "Sakshi"
+let nametwo = name;
+nametwo = "Shalini"
+console.log(name)
+console.log(nametwo)
+
+const obj = {
+    name:"sakshi",
+    email:"sakshi@gmail.com",
+
+}
+let objtwo = obj;
+objtwo.name = "Shalini"
+console.log(obj)
+console.log(objtwo)
