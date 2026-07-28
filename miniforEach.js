37edc02fc20efed8488fc884aa22521d1281fff0
Rohit-Chaubey-1 <rohitchaubey9876@gmail.com>
2026-07-28 20:49:52 +0530

    Filer

--- miniforEach.js
+++ miniforEach.js
@@ -1,11 +1,36 @@
-function square(value){
-    console.log(value*value)
+// function show(value,index,arr){
+//     console.log(value,index,arr)
+// }
+// function myforEach(arr,callback){
+//    for(let i=0;i<arr.length;i++){
+//        let index = i;
+//       callback(arr[i],index,arr)
+//    }
+
+// }
+
+// myforEach([2,3,4],show)
+//let arr = [10,20,30]
+// arr.forEach((dog,cat,rat)=>{
+//    console.log(dog)
+//    console.log(cat)
+//    console.log(rat)
+
+// })
+
+
+function show(value,index,array){
+    console.log(value,index,array)
+    
 }
-function myforEach(arr,callback){
-   for(let i=0;i<arr.length;i++){
-      callback(arr[i])
-   }
+function myMap(arr,callback){
+    let result = [];
+    for(let i=0;i<arr.length;i++){
+        const returnValue = callback(arr[i],i,arr)
+        result.push(returnValue);
+
+    }
+    return result;
 
 }
-
-myforEach([2,3,4],square)
+myMap([1,2,3],show)
