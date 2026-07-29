71c08f95c5da3160b690a8074a17ed0afe5d0e74
Rohit-Chaubey-1 <rohitchaubey9876@gmail.com>
2026-07-29 18:04:06 +0530

    find

---
+++ find.js
@@ -0,0 +1,27 @@
+function show(value,index,arr){
+  return(value>10)
+}
+
+
+function myfind(arr,callback){
+        
+    for(let i=0;i<arr.length;i++){
+        let found=callback(arr[i],i,arr);
+        if(found){
+            return arr[i]
+            
+        }
+
+    }
+    return undefined;
+
+}
+let result = myfind([1,2,10,12,16,19],show)
+console.log(result);
+
+//using find method
+let arr = [2,3,4,5,6]
+let found = arr.find((value)=>{
+    return value>3
+})
+console.log(found)
