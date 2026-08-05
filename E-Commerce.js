1f6c424ba0223c7a9ac5da90dc0640d4b1bafdc3
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-08-05 19:15:48 +0530

    E-commerce

---
+++ E-Commerce.js
@@ -0,0 +1,32 @@
+const orders = [
+  {
+    id: 101,
+    customer: "Alice",
+    city: "Delhi",
+    items: [
+      { name: "Laptop", price: 50000, qty: 1 },
+      { name: "Mouse", price: 800, qty: 2 }
+    ],
+    delivered: true
+  },
+  {
+    id: 102,
+    customer: "Bob",
+    city: "Mumbai",
+    items: [
+      { name: "Keyboard", price: 2500, qty: 1 },
+      { name: "Monitor", price: 15000, qty: 2 }
+    ],
+    delivered: false
+  },
+  {
+    id: 103,
+    customer: "Charlie",
+    city: "Delhi",
+    items: [
+      { name: "Mouse", price: 800, qty: 3 },
+      { name: "Laptop", price: 50000, qty: 1 }
+    ],
+    delivered: true
+  }
+];
--- Intermediate.js
+++ Intermediate.js
@@ -35,5 +35,29 @@ const users = [
 ];
 
 
-let ActiveUsers = users.filter(user=>user.isActive==true).sort((a,b)=>a.age-b.age).map(user=>user.name).sort(user=>user.age)
+let ActiveUsers = users.filter(user=>user.isActive==true).sort((a,b)=>a.age-b.age).map(user=>{return user.name
+    console.log(user.name)
+}
+)
 console.log(ActiveUsers)
+
+let passwords = [
+    "hello123",
+    "admin",
+    "OpenAI@2025",
+    "abcd"
+];
+
+let findPassword = passwords.some(pass=>pass.includes("@"))
+console.log(findPassword)
+
+let products = [
+    { name: "Laptop", price: 60000, inStock: true },
+    { name: "Phone", price: 30000, inStock: false },
+    { name: "Watch", price: 10000, inStock: true },
+    { name: "Tablet", price: 25000, inStock: true }
+];
+let InstockProducts = products.filter(pro=>pro.inStock==true).reduce((acc,curr)=>{
+    return acc+curr.price
+},0)
+console.log(InstockProducts)
