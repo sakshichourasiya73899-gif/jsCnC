e6097d353bef63cd05e16a900f406ba2cbcba308
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-08-05 15:05:38 +0530

    Intermediate

--- Intermediate.js
+++ Intermediate.js
@@ -1,9 +1,39 @@
-let users = [
-    {name:"Alice", age:20},
-    {name:"Bob", age:17},
-    {name:"Charlie", age:25}
-];
-//Get the names of adults only "If I want the Array then use map"
-let getAdult = users.filter(user=>user.age>=18).map(user=>user.name)
-console.log(getAdult)
+// let users = [
+//     {name:"Alice", age:20},
+//     {name:"Bob", age:17},
+//     {name:"Charlie", age:25}
+// ];
+// //Get the names of adults only "If I want the Array then use map"
+// let getAdult = users.filter(user=>user.age>=18).map(user=>user.name)
+// console.log(getAdult)
 
+const users = [
+    {
+        id: 1,
+        name: "Alice",
+        age: 20,
+        city: "Delhi",
+        isActive: true,
+        role: "User"
+    },
+    {
+        id: 2,
+        name: "Bob",
+        age: 17,
+        city: "Mumbai",
+        isActive: false,
+        role: "Admin"
+    },
+    {
+        id: 3,
+        name: "Charlie",
+        age: 25,
+        city: "Pune",
+        isActive: true,
+        role: "User"
+    }
+];
+
+
+let ActiveUsers = users.filter(user=>user.isActive==true).map(user=>user.name)
+console.log(ActiveUsers)
