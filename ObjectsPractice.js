64db7799f948e35e58ce62befaffdcf63ccd8084
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-08-08 00:40:59 +0530

    ObjectPractice

---
+++ ObjectsPractice.js
@@ -0,0 +1,15 @@
+const jsUser = {
+    name:"Sakshi",
+    age:20,
+    location:"Jaipur",
+    email:"sakshi@gmail.com",
+    isloggedIn:false,
+    lastloginDays:["Monday","Saturday"]
+
+
+}
+
+console.log(jsUser.name)
+console.log(jsUser["email"])
+
+//Object.create   this is the constructor method of creating an object 
