f32d718eb66c85c63efbd2351982690c2eb9f976
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-09-12 22:54:27 +0530

    addition of elements in array

--- functions.js
+++ functions.js
@@ -1,17 +1,23 @@
-function addTwoNumbers(number1,number2){
-    return (number1+number2)
-  // console.log( number1+number2)
-}
-let result = addTwoNumbers(3,4)
-console.log(result)
+// function addTwoNumbers(number1,number2){
+//     return (number1+number2)
+//   // console.log( number1+number2)
+// }
+// let result = addTwoNumbers(3,4)
+// console.log(result)
 
-function loginUserMessage(username="sam"){
-    if(!username){
-        console.log("Please enter a username")
-        return
-    }
-    return `${username} just loggedIn`
+// function loginUserMessage(username="sam"){
+//     if(!username){
+//         console.log("Please enter a username")
+//         return
+//     }
+//     return `${username} just loggedIn`
+// }
+// // let loginUser = loginUserMessage("")
+// // console.log(loginUser)
+// console.log(loginUserMessage("Sakshi"))
+
+//function declaration 
+function test(){
+    console.log("This is being tested");
 }
-// let loginUser = loginUserMessage("")
-// console.log(loginUser)
-console.log(loginUserMessage("Sakshi"))
+test();
--- functionshoisting.js
+++ functionshoisting.js
@@ -1,10 +1,27 @@
-//Function declarations are fully hoisted — you can call them before they appear in code.
-console.log(fact())
-function fact(){
-  return "factorial"
+// //Function declarations are fully hoisted — you can call them before they appear in code.
+// console.log(fact())
+// function fact(){
+//   return "factorial"
+// }
+
+// // Function expressions (including arrow functions) are NOT hoisted the same way — the variable is hoisted but stays undefined until the assignment line runs (TDZ if let/const).
+
+// // console.log(age)
+// // let age = "21"
+
+// console.log(num)
+// var num = 10;
+
+
+
+// function declaration 
+function test(){
+  console.log("tested.....");
 }
+test();
 
-// Function expressions (including arrow functions) are NOT hoisted the same way — the variable is hoisted but stays undefined until the assignment line runs (TDZ if let/const).
-
-console.log(age)
-let age = "21"
+// function expression
+const square = function(){
+  console.log("square of number")
+}
+square();
